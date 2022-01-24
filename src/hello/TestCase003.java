4e6c185e601decbbeda2fb03290478dcbb105b7f
Anton Smirnov <antonsmirnov4u@yahoo.com>
2022-01-24 11:49:35 -0500

    TestCase003 more text added

--- src/hello/TestCase003.java
+++ src/hello/TestCase003.java
@@ -5,5 +5,6 @@ public class TestCase003 {
     public static void main(String[] args) {
 
         System.out.println("Blah blah text");
+        System.out.println("more text for commit");
     }
 }
