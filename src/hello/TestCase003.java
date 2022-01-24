f3fd907eb2b0467791c4a3961bd3793455eca339
Anton Smirnov <antonsmirnov4u@yahoo.com>
2022-01-24 12:46:02 -0500

    TestCase003 changed in master

--- src/hello/TestCase003.java
+++ src/hello/TestCase003.java
@@ -7,5 +7,7 @@ public class TestCase003 {
         System.out.println("Blah blah text");
         System.out.println("more text for commit");
         System.out.println("also more stuff");
+
+        //these are the changes from intellij
     }
 }
