62068f807a420bed96358c006635aae67d81d9a0
Anton Smirnov <antonsmirnov4u@yahoo.com>
2022-01-24 11:21:54 -0500

    TestCase002 3rd commit

--- src/hello/TestCase002.java
+++ src/hello/TestCase002.java
@@ -7,5 +7,7 @@ public class TestCase002 {
         System.out.println("You ever been in a tesla?");
 
         System.out.println("damn...");
+
+        System.out.println("siri, get this dude outta my car");
     }
 }
